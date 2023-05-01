95c684f201e58292de58f885e2cc3b3e53b66569
C-Hurtmann <constantine2903@gmail.com>
2023-05-01 11:39:56 +0300

    add cli command parser

--- core/exchange.py
+++ core/exchange.py
@@ -1,29 +1,34 @@
+import argparse
 import aiohttp
 import asyncio
-import sys
 from datetime import datetime, timedelta
 from time import time
 from pprint import pprint
 
+
 class NegativeDaysError(Exception):
     pass
 
-class Request:
 
-    async def get_json(self, session):
-        print('get_json started')
-        start = time()
-        async with session.get(self.url) as response:
-            print(f'get_json connection: {time() - start}')
-            result = await response.json()
-            print(f'get_json final: {time() - start}')
-            return result
+class Request:
+    async def get_json(self, session: aiohttp.ClientSession):
+        try:
+            async with session.get(
+                self.url
+            ) as resp:  #  The main delay in connection here
+                if resp.status == 200:
+                    result = await resp.json()
+                else:
+                    print(f"Error status {resp.status} for {self.url}")
+                return result
+        except aiohttp.ClientConnectionError as err:
+            print(f"Connection error: {self.url}", str(err))
 
     def __repr__(self):
         return self.url
 
-class ExchangeRequest(Request):
 
+class ExchangeRequest(Request):
     url = "https://api.privatbank.ua/p24api/exchange_rates?json"
 
     def __init__(self, days):
@@ -31,19 +36,17 @@ class ExchangeRequest(Request):
 
     def update_url(self, days):
         now = datetime.now()
-        if 0 <= days <= 10:
-            date = (now - timedelta(days=days))
+        if days <= 10:
+            date = now - timedelta(days=days)
         elif days > 10:
-            date = (now - timedelta(days=10)) # when days more then 10
-        else:
-            raise NegativeDaysError
-        formated_date  = date.strftime('%d.%m.%Y')
-        self.url = self.url + '&date=' + formated_date
+            date = now - timedelta(days=10)  # when days more then 10
+        formated_date = date.strftime("%d.%m.%Y")
+        self.url = self.url + "&date=" + formated_date
 
 
-class JokeRequest(Request): # Only for testing. Don't pay attention on this.
+class JokeRequest(Request):  # Only for testing. Don't pay attention on this.
     url = "https://icanhazdadjoke.com/"
-    headers = {"Accept": 'application/json'}
+    headers = {"Accept": "application/json"}
 
     async def get_value(self):
         json = await self._get_json()
@@ -51,33 +54,48 @@ class JokeRequest(Request): # Only for testing. Don't pay attention on this.
 
 
 class RequestHandler:
-
     async def get_exchange_rate(self, days):
         async with aiohttp.ClientSession() as session:
-            start = time()
-            print('get_exchange_rate started')
             requests = [ExchangeRequest(i).get_json(session) for i in range(days)]
             result = await asyncio.gather(*requests)
-            print(f'get_exchange_rate: {time() - start}')
             return result
 
 
-
 def main(days=1, add_currency=None):
-    selected_currencies = ['USD', 'EUR'] + [add_currency]
+    selected_currencies = ["USD", "EUR"] + [add_currency]
     start = time()
     jsons = asyncio.run(RequestHandler().get_exchange_rate(days))
     formated_jsons = []
     for json in jsons:
-        date = json['date']
-        values_preset = list(filter(lambda x: x['currency'] in selected_currencies, json['exchangeRate']))
-        formated_values  ={}
+        date = json["date"]
+        values_preset = list(
+            filter(lambda x: x["currency"] in selected_currencies, json["exchangeRate"])
+        )
+        formated_values = {}
         for i in values_preset:
-            d = {i['currency']: {'purchase': i['purchaseRate'], 'sale': i['saleRate']}}
+            d = {i["currency"]: {"purchase": i["purchaseRate"], "sale": i["saleRate"]}}
             formated_values.update(d)
         formated_jsons.append({date: formated_values})
-    pprint(formated_jsons)
-    print(time() - start)
+    return formated_jsons
 
-if __name__ =='__main__':
-    main(2)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Creates report of currenices exchange rate. If not args, create by today and only for USD adn EUR"
+    )
+    parser.add_argument(
+        "-d",
+        "--days",
+        type=int,
+        default=1,
+        help="Creates additional reports for chosen number of days from today counting back",
+    )
+    parser.add_argument(
+        "-a",
+        "--add",
+        type=str,
+        default=None,
+        help="Extend report by chosen currency"
+    )
+    namespace = parser.parse_args()
+    pprint(main(days=namespace.days, add_currency=namespace.add))
